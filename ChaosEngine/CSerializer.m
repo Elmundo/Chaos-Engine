4c2d0c16b74930b3f4bca932a16224e18b836748
Baris YILMAZ <baris.yilmaz.1985@gmail.com>
2014-02-10 17:54:03 +0200

    CSerializer must go on

--- ChaosEngine/CSerializer.m
+++ ChaosEngine/CSerializer.m
@@ -31,6 +31,10 @@
 {
     /* TBXElement could be three type: Entity, Template, Group */
     
+    /* Delete later */
+    char *name = @encode(char);
+    fprintf(stdout, "name= %s\n", name);
+    
     TBXMLElement *currentElement = xml;
     while (currentElement) {
         
@@ -52,7 +56,7 @@
 - (id)traverseObject:(TBXMLElement *)xml object:(id)object
 {
     TBXMLElement *currentElement = xml;
-    [self setProperties: object];
+    [self setProperties: object xml:currentElement];
     
     TBXMLElement *childElement = currentElement->firstChild;
     while (childElement) {
@@ -67,7 +71,7 @@
     return nil;
 }
 
-- (void)setProperties:(id)object
+- (void)setProperties:(id)object xml:(TBXMLElement *)element
 {
     unsigned int outCount, i;
     objc_property_t *properties = class_copyPropertyList([object class], &outCount);
@@ -77,13 +81,17 @@
         const char * propName = property_getName(property);
         if (propName) {
             NSString *propType = [self propertyTypeStringOfProperty:property];
-            
-            NSString *propertyName = [NSString stringWithUTF8String:propName];
-            NSString *propertyType = [NSString stringWithString:propType];
-
-            Class classObj = NSClassFromString(propertyType);
-            id obj = [[classObj alloc] init];
-            [object setValue:obj forKey:propertyName];
+            if ([self isScalarType:propType]) {
+                [self setScalarValue:object xml:element propertyType:propType];
+            }
+            else{ //Object to be initiated
+                NSString *propertyName = [NSString stringWithUTF8String:propName];
+                NSString *propertyType = [NSString stringWithString:propType];
+                
+                Class classObj = NSClassFromString(propertyType);
+                id obj = [[classObj alloc] init];
+                [object setValue:obj forKey:propertyName];
+            }
             
         }
     }
@@ -91,20 +99,88 @@
     free(properties);
 }
 
+- (void)setScalarValue:(id)obj xml:(TBXMLElement *)element propertyType:(NSString *)type
+{
+    //Scalar types
+    if ([type isEqualToString:@"c"]) {
+        NSString *string =  [element->text];
+        char c =
+    }else if([type isEqualToString:@"i"])
+    {
+        
+    }else if([type isEqualToString:@"f"]){
+        
+    }
+}
+
+- (BOOL)isScalarType:(NSString *)type
+{
+    if ([type isEqualToString:@"i"] || [type isEqualToString:@"c"] || [type isEqualToString:@"f"] ||
+        [type isEqualToString:@"NSString"]  || [type isEqualToString:@"NSArray"] || [type isEqualToString:@"NSDictionary"] ||
+        [type isEqualToString:@"NSNumber"] || [type isEqualToString:@"NSDecimalNumber"] || [type isEqualToString:@"NSDate"] ||
+        [type isEqualToString:@"NSURL"]) {
+        return YES;
+    }
+    return NO;
+}
+
+- (NSString *)propertyTypeStringOfProperty:(objc_property_t) property
+{
+    const char *attributes = property_getAttributes(property);
+    //printf("attributes=%s\n", attributes);
+    char buffer[1 + strlen(attributes)];
+    strcpy(buffer, attributes);
+    char *state = buffer, *attribute;
+    while ((attribute = strsep(&state, ",")) != NULL) {
+        if (attribute[0] == 'T' && attribute[1] != '@') {
+            // it's a C primitive type:
+            /*
+             if you want a list of what will be returned for these primitives, search online for
+             "objective-c" "Property Attribute Description Examples"
+             apple docs list plenty of examples of what you get for int "i", long "l", unsigned "I", struct, etc.
+             */
+            NSString *name = [[NSString alloc] initWithBytes:attribute + 1 length:strlen(attribute) - 1 encoding:NSASCIIStringEncoding];
+            return name;
+        }
+        else if (attribute[0] == 'T' && attribute[1] == '@' && strlen(attribute) == 2) {
+            // it's an ObjC id type:
+            return @"id";
+        }
+        else if (attribute[0] == 'T' && attribute[1] == '@') {
+            // it's another ObjC object type:
+            NSString *name = [[NSString alloc] initWithBytes:attribute + 3 length:strlen(attribute) - 4 encoding:NSASCIIStringEncoding];
+            return name;
+        }
+    }
+    return nil;
+}
+
+/*
 - (NSString *)propertyTypeStringOfProperty:(objc_property_t) property {
     const char *attr = property_getAttributes(property);
     NSString *const attributes = [NSString stringWithCString:attr encoding:NSUTF8StringEncoding];
+    fprintf(stdout, "%s %s\n", property_getName(property), property_getAttributes(property));
     
     NSRange const typeRangeStart = [attributes rangeOfString:@"T@\""];  // start of type string
-    if (typeRangeStart.location != NSNotFound) {
+    if (typeRangeStart.location != NSNotFound) { // Type is not
         NSString *const typeStringWithQuote = [attributes substringFromIndex:typeRangeStart.location + typeRangeStart.length];
         NSRange const typeRangeEnd = [typeStringWithQuote rangeOfString:@"\""]; // end of type string
         if (typeRangeEnd.location != NSNotFound) {
             NSString *const typeString = [typeStringWithQuote substringToIndex:typeRangeEnd.location];
             return typeString;
         }
+    }else if(){
+        
+    }
+    else
+    {
+        NSRange const scalarTypeRangeStart = [attributes rangeOfString:@"T"];
+        NSString *const scalarTypeString = [attributes substringFromIndex:scalarTypeRangeStart.location + typeRangeStart.length + 1];
+        NSRange const scalarTypeRangeEnd = [scalarTypeString rangeOfString:@","]; // end of type string
+        NSString *const scalarType = [scalarTypeString substringToIndex:scalarTypeRangeEnd.location];
+        return scalarType;
     }
     return nil;
 }
-
+*/
 @end
--- ChaosEngine/CTestComponent.h
+++ ChaosEngine/CTestComponent.h
@@ -11,8 +11,21 @@
 
 @interface CTestComponent : CComponent
 
+// Object Type
 @property (nonatomic, strong) CPoint *position;
 @property (nonatomic, strong) NSString *name;
 @property (nonatomic, strong) NSString *surename;
+@property (nonatomic, strong) NSArray *list;
+@property (nonatomic, strong) NSDictionary *dic;
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSDecimalNumber *decimalNumber;
+@property (nonatomic, strong) NSNumber *number;
+@property (nonatomic, strong) id object;
+
+//Scalar and Structure Type
+@property (nonatomic, assign) int x;
+@property (nonatomic, assign) float y;
+@property (nonatomic, assign) long l;
+@property (nonatomic, assign) char c;
 
 @end
